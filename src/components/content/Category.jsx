24c7588def4742e908f8de5df0d2bfffa345b8a3
unknown <juanmeotero@gmail.com>
2022-10-08 18:45:57 -0300

    Agregando productos, modificando diseño de categories

--- src/components/content/Category.jsx
+++ src/components/content/Category.jsx
@@ -15,7 +15,7 @@ const Category = () => {
           <img src={"../img/" + product.img} alt={product.product} className="img-fluid p-0 rounded p-1" />
           <div className="card-body text-center">
             <h6 className="card-title">{product.product}</h6>
-            <h6 className="card-subtitle text-muted">{product.model}</h6>
+            <h6 className="card-subtitle text-muted">${product.value}</h6>
           </div>
           <Link to={"/product/" + product.id}><button className='btn btn-dark w-100 border-0'>Ver producto</button></Link>
         </div>
@@ -26,7 +26,7 @@ const Category = () => {
 
   return (
     <>
-      <div className='container-fluid d-flex justify-content-center my-3'>
+      <div className='container d-flex justify-content-center my-3'>
         <div className='row container-fluid d-flex ms-4'>
           {categories}
         </div>
--- src/components/content/ItemListContainer.jsx
+++ src/components/content/ItemListContainer.jsx
@@ -11,10 +11,10 @@ const Home = () => {
         <div className="card mb-3 cardProduct p-0" key={product.id}>
           <img src={"./img/" + product.img} alt={product.product} className="img-fluid p-0 rounded p-1" />
           <div className="card-body text-center">
-            <h6 className="card-title">{product.product}</h6>
-            <h6 className="card-subtitle text-muted">{product.model}</h6>
+            <h6 className="card-title h-75 text-color-light text-white">{product.product}</h6>
+            <h6 className="card-subtitle mt-3 text-light">${product.value}</h6>
           </div>
-          <Link to={"/product/" + product.id}><button className='btn btn-dark w-100 border-0'>Ver producto</button></Link>
+          <Link to={"/product/" + product.id}><button className='btn btn-dark w-100 border-0 mt-3'>Ver producto</button></Link>
         </div>
       )
       setProducts(cardProduct)
